d517342c298381209dbb8a9cddb6c8569646ec3f
Asifur Rahman <asifurrahman.ac@gmail.com>
2026-06-08 19:31:08 +0600

    scroll bar hidden

--- src/BasicComponent/MarQuee.jsx
+++ src/BasicComponent/MarQuee.jsx
@@ -3,8 +3,8 @@ import Marquee from "react-fast-marquee";
 
 const MarQuee = () => {
   return (
-    <Marquee className="py-1"  speed={40}>
-      <p className="px-5 font-bold">Student must return the book within 3 Days !</p>
+    <Marquee className=""  speed={40}>
+      <p className="px-5">Student must return the book within 3 Days !</p>
     </Marquee>
   );
 };
--- src/BasicComponent/NavBar.jsx
+++ src/BasicComponent/NavBar.jsx
@@ -11,6 +11,7 @@ const NavBar = () => {
   const { user, Auth } = useContext(Context);
   const [Dropdown, setDropdown] = useState(false);
   const login = useNavigate();
+  const navigate = useNavigate()
 
   function signout() {
     signOut(Auth).then((res) => {
@@ -18,111 +19,87 @@ const NavBar = () => {
     });
   }
   return (
-    <section>
-      <div className="w-full flex items-center lg:px-6 md:px-6 px-3 h-16">
-        
-        
-        <div className="navbar-end">
-          
-
-          {user && (
-            <img
-              id="image_tooltip"
-              src={user?.photoURL ? user?.photoURL : personIcon}
-              className="mx-2 h-[40px] w-[40px] rounded-full "
-              alt="image"
-            />
-          )}
-          <Tooltip
-            anchorSelect="#image_tooltip"
-            content={user?.displayName ? user?.displayName : "Unknown"}
-            place="left"
-          ></Tooltip>
-          {user ? (
-            <button onClick={signout} className="btn-hover color-5">
-              SignOut
-            </button>
-          ) : (
-            <button
-              onClick={() => {
-                login("/login");
-              }}
-              className="btn-hover color-5"
+    <div className="navbar shadow-sm">
+      <div className="navbar-start">
+        <div
+          className="dropdown"
+          tabIndex={0}
+          onClick={() => setDropdown(!Dropdown)}
+        >
+          <div className="btn btn-ghost btn-circle">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className="h-5 w-5"
+              fill="none"
+              viewBox="0 0 24 24"
+              stroke="currentColor"
             >
-              SignIn
-            </button>
+              {" "}
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h8m-8 6h16"
+              />{" "}
+            </svg>
+          </div>
+          {Dropdown && (
+            <ul
+              tabIndex={0}
+              className="menu menu-sm bg-base-100 dropdown-content rounded-box z-1 mt-3 w-40 p-2 shadow"
+            >
+              <li onClick={() => setDropdown(false)}>
+                <Link>Home</Link>
+              </li>
+
+              <li onClick={() => setDropdown(false)}>
+                <Link to={`/allbooks/all`}>All Books</Link>
+              </li>
+
+              {user && (
+                <li onClick={() => setDropdown(false)}>
+                  <Link to={"/addbook"}>Add Book</Link>
+                </li>
+              )}
+              {user && (
+                <li onClick={() => setDropdown(false)}>
+                  <Link to={"/borrowedbooks"}>Borrowed</Link>
+                </li>
+              )}
+              {user ? (
+                <li onclick={signout}>
+                  <a href="#">SignOut</a>
+                </li>
+              ) : (
+                <li onClick={() => login("/login")}>
+                  <a href="#">SignIn</a>
+                </li>
+              )}
+            </ul>
           )}
         </div>
       </div>
-      <div className="navbar bg-base-100 shadow-sm">
-        <div className="navbar-start">
-          <div
-            className="dropdown"
-            tabIndex={0}
-            onClick={() => setDropdown(!Dropdown)}
-          >
-            <div className="btn btn-ghost btn-circle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                className="h-5 w-5"
-                fill="none"
-                viewBox="0 0 24 24"
-                stroke="currentColor"
-              >
-                {" "}
-                <path
-                  strokeLinecap="round"
-                  strokeLinejoin="round"
-                  strokeWidth="2"
-                  d="M4 6h16M4 12h8m-8 6h16"
-                />{" "}
-              </svg>
-            </div>
-            {Dropdown && (
-              <ul
-                tabIndex={0}
-                className="menu menu-sm bg-base-100 dropdown-content rounded-box z-1 mt-3 w-40 p-2 shadow"
-              >
-                <li onClick={() => setDropdown(false)}>
-                  <Link>Home</Link>
-                </li>
-
-                <li onClick={() => setDropdown(false)}>
-                  <Link to={`/allbooks/all`}>All Books</Link>
-                </li>
-
-                {user && (
-                  <li onClick={() => setDropdown(false)}>
-                    <Link to={"/addbook"}>Add Book</Link>
-                  </li>
-                )}
-                {user && (
-                  <li onClick={() => setDropdown(false)}>
-                    <Link to={"/borrowedbooks"}>Borrowed</Link>
-                  </li>
-                )}
-                {user ? (
-                  <li onclick={signout}>
-                    <a href="#">SignOut</a>
-                  </li>
-                ) : (
-                  <li onClick={() => login("/login")}>
-                    <a href="#">SignIn</a>
-                  </li>
-                )}
-              </ul>
-            )}
-          </div>
-        </div>
-        <div className="navbar-center">
-          <p className="text-3xl  font-extrabold text-[#30d787] ">LitHub</p>
-        </div>
-        {/* completed */}
-        <div className="navbar-end">
-          <ThemeController></ThemeController>
-        </div>
+      <div className="navbar-center">
+        <p onClick={()=>navigate("/")} className="text-3xl  font-extrabold text-library cursor-pointer">LitHub</p>
       </div>
-    </section>
+      {/* completed */}
+      <div className="navbar-end">
+        {user && (
+          <img
+            id="image_tooltip"
+            src={user?.photoURL ? user?.photoURL : personIcon}
+            className="mx-2 h-[40px] w-[40px] rounded-full hidden md:inline"
+            alt="image"
+          />
+        )}
+        <Tooltip
+          anchorSelect="#image_tooltip"
+          content={user?.displayName ? user?.displayName : "Unknown"}
+          place="left"
+        ></Tooltip>
+        <ThemeController></ThemeController>
+      </div>
+    </div>
   );
 };
 
